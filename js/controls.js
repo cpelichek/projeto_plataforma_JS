6673908109371bd17f96c63cbc1ae5d6472c78ae
André Monello <atmonello@outlook.com>
2018-01-19 15:07:43 -0200

    Botão de Fullscreen funcional.

--- js/controls.js
+++ js/controls.js
@@ -1,7 +1,17 @@
 let botaoFullscreen = document.getElementById("fullscreenBtn");
+let isFullscreen = false;
 
 let toggleFullscreen = function () {
-  document.getElementById("gameWindow").className += " fullscreen";
+  let _gameWindow = document.getElementById("gameWindow");
+  if (!isFullscreen) {
+    _gameWindow.classList.add("enterFullscreen");
+    isFullscreen = true;
+  }
+
+  else {
+    _gameWindow.classList.remove("enterFullscreen");
+    isFullscreen = false;
+  }
 }
 
 botaoFullscreen.addEventListener("click", toggleFullscreen);
